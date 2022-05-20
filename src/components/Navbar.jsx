076a74ed8b257364c67213afea1e33ab327bb6c1
ShiotaYurie <shiota.yurie@all-integration.org>
2022-05-20 14:46:21 +0900

    CreateView

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Search } from "./Search";
+
+export const Navber = () => {
+    return (
+            <>
+            <p>ナビゲーション</p>
+            <Search />
+            </>
+    );
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Search = () => {
+    return(
+        <>
+        <p>調べる</p>
+        </>
+    );
+}
